1fbf4906abdff42d41ba3b6bfdadcd81d5f9291f
Luke Pascale <lpluke5@gmail.com>
2022-04-05 20:53:21 -0500

    Refactor: Reduce size of shelters and pets spec files

--- spec/features/shelters/pets_index_spec.rb
+++ spec/features/shelters/pets_index_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe 'the shelters pets index' do
     @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
     @pet_3 = Pet.create(adoptable: true, age: 1, breed: 'domestic shorthair', name: 'Sylvester', shelter_id: @shelter_2.id)
     @pet_4 = Pet.create(adoptable: true, age: 1, breed: 'orange tabby shorthair', name: 'Lasagna', shelter_id: @shelter.id)
+
+    visit "/shelters/#{@shelter.id}/pets"
   end
 
   it 'lists all the pets associated with the shelter, with their attributes' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     expect(page).to have_content(@pet_1.name)
     expect(page).to have_content(@pet_1.breed)
@@ -28,7 +29,6 @@ RSpec.describe 'the shelters pets index' do
   end
 
   it 'displays a link to create a new pet' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     expect(page).to have_link("Create a Pet")
     click_on("Create a Pet")
@@ -36,7 +36,6 @@ RSpec.describe 'the shelters pets index' do
   end
 
   it 'displays a link to edit each pet' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     expect(page).to have_link("Edit #{@pet_1.name}")
     expect(page).to have_link("Edit #{@pet_2.name}")
@@ -47,7 +46,6 @@ RSpec.describe 'the shelters pets index' do
   end
 
   it 'displays a link to delete each pet' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     expect(page).to have_link("Delete #{@pet_1.name}")
     expect(page).to have_link("Delete #{@pet_2.name}")
@@ -59,14 +57,12 @@ RSpec.describe 'the shelters pets index' do
   end
 
   it 'displays a form for a number value' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     expect(page).to have_content("Only display pets with an age of at least...")
     expect(page).to have_select("age")
   end
 
   it 'only displays records above the given return value' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     find("#age option[value='3']").select_option
     click_button("Filter")
@@ -76,7 +72,6 @@ RSpec.describe 'the shelters pets index' do
   end
 
   it 'allows the user to sort in alphabetical order' do
-    visit "/shelters/#{@shelter.id}/pets"
 
     expect(@pet_1.name).to appear_before(@pet_2.name)
     expect(@pet_2.name).to appear_before(@pet_4.name)
